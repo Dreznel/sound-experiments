96a64b031424fffb45f13cdbe182c4677849151e
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-09-28 12:54:39 -0400

    Initial commit.

---
+++ src/com/dreznel/Main.java
@@ -0,0 +1,11 @@
+package com.dreznel;
+
+import javax.sound.sampled.*;
+import javax.sound.midi.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
